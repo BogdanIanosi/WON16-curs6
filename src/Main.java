44ecfbe4d83af922f3853c2f7aef67194a5d1cf8
Bogdan-Ianosi <bogdanianosi14@gmail.com>
2024-04-13 16:19:07 +0300

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("Hello world!");
+        String param = "test";
+        String s = new String(param);
+        String s2 = new String("alt test");
+
+        Clock c = new Clock();
+        c.setTime( 99999, 99999);
+        Clock c2 = new Clock();
+
+        Clock customClock = new Clock(3, 15);
+        System.out.println(customClock.getTime());
+        customClock.increaseMinute();
+        System.out.println(customClock.getTime());
     }
 }
